02128c51359f741e9ccd62a9317961f90128df40
Artur Shamsutdinov <artur.shamsutdinov@gmail.com>
2015-05-20 20:41:35 +1000

    Initial commit

---
+++ ThemeTest/ThemeTest/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace ThemeTest
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            SetDefaultStyle();
+            MainPage = new TestPage();
+        }
+
+        public void SetDefaultStyle()
+        {
+            Resources = Default;
+        }
+
+        public void SetSecondStyle()
+        {
+            Resources = Second;
+        }
+    }
+}
---
+++ ThemeTest/ThemeTest/TestPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace ThemeTest
+{
+    public partial class TestPage : ContentPage
+    {
+        public TestPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnDefaultStyle_OnClicked(object sender, EventArgs e)
+        {
+            ((App)App.Current).SetDefaultStyle();
+        }
+
+        private void BtnSecondStyle_OnClicked(object sender, EventArgs e)
+        {
+            ((App)App.Current).SetSecondStyle();
+        }
+    }
+}
